b465d82a83d7a000944604402604584e7a8077f5
Chimezie Ndukwe <domcndukwe@gmail.com>
2026-05-03 17:59:38 -0400

    version13

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 //template_krcay8n
 //service_4zyaozo
 //5B9m400C7RuSN3GVi
-
+/*
 function contact(event) {
   event.preventDefault();
 
@@ -16,3 +16,23 @@ function contact(event) {
     console.log(error);
   });
 }
+  */
+
+function contact(event) {
+  event.preventDefault();
+  console.log('Form submitted');
+
+  emailjs.sendForm(
+    'service_4zyaozo',
+    'template_krcay8n',
+    event.target,
+    '5B9m400C7RuSN3GVi'
+    ).then(() => {
+      console.log('It worked');
+      alert('Message sent successfully!');
+    }).catch((error) => {
+      console.log("EmailJS error:", error);
+      alert("Message failed.");
+    });
+  }
+    
